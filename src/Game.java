bc7e835af6ca097368406eb0dc2df98ab97d0018
Max Stevenson <maxstevenson@msn.com>
2019-01-22 15:58:48 +0000

    First commit: - Experimentation with Scene transition. - Game now opens on menu screen, with button click to progress to main game screen. - All logic contained within main Game class - lots of scope for refactoring.

--- src/Game.java
+++ src/Game.java
@@ -3,14 +3,19 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+
 
 public class Game extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Release the Kracken");
+
         VBox root = new VBox();
 
         Board p1Board = new Board(8);
@@ -21,9 +26,22 @@ public class Game extends Application {
         root.setAlignment(Pos.CENTER);
         root.setPadding(new Insets(10, 10, 10, 10));
         root.setSpacing(10);
-
         Scene scene1 = new Scene(root, 320, 640);
-        primaryStage.setScene(scene1);
+
+        VBox welcome = new VBox();
+
+        Label label = new Label("Welcome to Battleships");
+        Button button1 = new Button("Play game");
+        button1.setOnAction(actionEvent ->  {
+            primaryStage.setScene(scene1);
+        });
+
+        welcome.getChildren().add(label);
+        welcome.getChildren().add(button1);
+
+        Scene scene0 = new Scene(welcome, 320, 640);
+
+        primaryStage.setScene(scene0);
         primaryStage.show();
     }
 
