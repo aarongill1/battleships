abf6192654c0493531da34507b1bfb6a470d4f6c
Ajay <ajaymungur@hotmail.com>
2019-01-30 22:28:17 +0000

    can place kraken and get his coordinates

--- src/Model/Board.java
+++ src/Model/Board.java
@@ -139,6 +139,20 @@ public class Board {
         return result;
     }
 
+    public String populateBoardByKraken() {
+        // Gets ship coordinates
+        String result = "";
+        for (int i = 0; i < this.getSize(); i++) {
+            for (int j = 0; j < this.getSize(); j++) {
+                if (this.getTileList().get(i).get(j).isKraken()) {
+                    result += i;
+                    result += j;
+                }
+            }
+        }
+        return result;
+    }
+
     public void drawBoard(String coords, ImagePattern fill) {
         // Use coordinates to place ships in existing board
         coords = coords.replaceAll("[^\\d.]", "");
